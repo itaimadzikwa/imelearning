7fec918aefd783bc8fbfc6ed1f8ce414cbbfe128
itaimadzikwa <tanyaradzwachiko92@gmail.com>
2026-05-16 20:27:31 +0200

    Update V1.0

--- calculator.js
+++
@@ -1,9 +0,0 @@
-function calculate(){
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-
-    let answer = num1 + num2;
-
-    document.getElementById("result").innerHTML =
-    "Answer: " + answer;
-}
